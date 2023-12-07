d62f3ea2a97ca80ee0132445991f20ff90e74f7e
programistica <evenogueiratecnologia@gmail.com>
2023-12-06 23:11:02 -0300

    refactor(backend): update the return value for the /carros route to correct the json format

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,13 +1,10 @@
 from . import db
 
 
-class Carro(db.Model):
+class Carros(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(255))
     marca = db.Column(db.String(255))
     ano = db.Column(db.Integer)
     preco = db.Column(db.Float)
     img = db.Column(db.String(255))
-
-    def __repr__(self):
-        return "<Carro %r>" % self.nome
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,29 @@
+import json
+
+import pandas as pd
 from flask import jsonify, request
 
 from . import app, db
-from .models import Carro
+from .models import Carros
 
 
 @app.route("/carros", methods=["GET"])
-def get_carros():
-    carros = Carro.query.all()
-    return jsonify([carro.serialize() for carro in carros])
+def get_Carros():
+    carros = Carros.query.all()
+
+    return (
+        jsonify(
+            [
+                {
+                    "id": carro.id,
+                    "nome": carro.nome,
+                    "marca": carro.marca,
+                    "ano": carro.ano,
+                    "preco": carro.preco,
+                    "img": carro.img,
+                }
+                for carro in carros
+            ]
+        ),
+        200,
+    )
